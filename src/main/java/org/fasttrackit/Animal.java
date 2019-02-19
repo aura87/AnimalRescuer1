92ac87863c1d9ff96582b187f02e162f73a789ef
Dog <dog@yahoo.com>
2019-02-19 19:23:51 +0000

    set-up basic project structure

---
+++ src/main/java/org/fasttrackit/Animal.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class Animal {
+
+    String name;
+    double age;
+    double healthLevel;
+    double hungerLevel;
+    double HappinessLevel;
+    String favoriteActivityName;
+    String favoriteFoodName;
+
+}
